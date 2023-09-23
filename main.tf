814ff96f0d27562c251dde8671c9b07b8bbaad85
MdMahediHassan <mahedi822002@gmail.com>
2023-09-24 00:41:47 +0800

    first terraform commit

--- main.tf
+++ main.tf
@@ -26,3 +26,15 @@ resource "azurerm_subnet" "myname" {
     depends_on = [ azurerm_virtual_network.myname ]
     resource_group_name = azurerm_resource_group.myname.name
 }
+
+resource "azurerm_network_interface" "myname" {
+    name = "nic-test"
+    location = azurerm_resource_group.myname.location
+    resource_group_name = azurerm_resource_group.myname.name
+    depends_on = [ azurerm_subnet.myname ]
+    ip_configuration {
+      name = "primary"
+      subnet_id = azurerm_subnet.myname.id
+      private_ip_address_allocation = "Dynamic"
+    }
+}
